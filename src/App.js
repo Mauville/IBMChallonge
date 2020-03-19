f8f0b37cc5ab5ab117c3289902bbb781278720c7
Mauville <4991127@outlook.com>
2020-03-18 22:30:12 -0600

    Create questionnaire

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// This is responsible for the design of the questionnaire
+
+import React from "react";
+import bill from "./img/bill.jpg";
+import arroz from "./img/arroz.jpg";
+import coffee from "./img/coffee.jpg";
+import light from "./img/light.png";
+
+import ImageInput from "./cpt/ImageInput.js";
+import "./App.css";
+import {
+  TextField,
+  Box,
+  Paper,
+  InputAdornment,
+  Container
+} from "@material-ui/core/";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" id="biggo">
+      <h1>Formulario para Cotización</h1>
+      <form>
+        {/* Datos Personales */}
+        <Paper elevation="2">
+          <Container>
+            <h5>Datos Personales</h5>
+            <TextField
+              label="Email"
+              required
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+            ></TextField>
+            <br />
+            <TextField
+              required
+              label="Código Postal"
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+            ></TextField>
+          </Container>
+        </Paper>
+        {/* Receipt */}
+        <Paper elevation="2">
+          <Container>
+            <h5>Informacion de Consumo</h5>
+            <Container maxWidth="sm">
+              <img className="bill" src={bill} alt="Bill"></img>
+            </Container>
+            <TextField
+              required
+              style={{ margin: 9 }}
+              data-persist="garlic"
+              label="Fecha de Inicio del Periodo"
+              type="date"
+              format="DD-MM-YYYY"
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+            <TextField
+              required
+              label="Energía del Primer Periodo"
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">kWh</InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              required
+              label="Energía del Segundo Periodo"
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">kWh</InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              required
+              label="Energía del Tercer Periodo"
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">kWh</InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              required
+              label="Energía del Cuarto Periodo"
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">kWh</InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              required
+              label="Energía del Quinto Periodo"
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">kWh</InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              required
+              label="Energía del Sexto Periodo"
+              data-persist="garlic"
+              style={{ margin: 9 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">kWh</InputAdornment>
+                )
+              }}
+            />
+          </Container>
+        </Paper>
+        {/* Appliances */}
+        <Paper elevation="2">
+          <Container>
+            <h5> ¿Con cuántos de los siguientes electrodomésticos cuenta usted? </h5>
+            <Box display="flex" justifyContent="space-around">
+              <ImageInput title="Arrocera" alt="Arrocera" src={arroz} />
+              <ImageInput title="Cafetera" alt="Cafetera" src={coffee} />
+              <ImageInput title="Foco Circular" alt="Foco Circular" src={light} />
+            </Box>
+          </Container>
+        </Paper>
+      </form>
+    </Container>
   );
 }
 
---
+++ src/cpt/ImageInput.js
@@ -0,0 +1,21 @@
+// This is a custom made component used to create the little image cards
+import React from "react";
+import "../css/ImageInput.css";
+import { TextField, Paper, Container } from "@material-ui/core/";
+
+export default function ImageInput(props) {
+  return (
+    <Paper className="cont">
+      <Container maxWidth="sm">
+        <h4>{props.title}</h4>
+        <img alt={props.alt} src={props.src} id="image"></img>
+        <br />
+        <TextField
+          required
+          style={{ margin: 9 }}
+          data-persist="garlic"
+        />
+      </Container>
+    </Paper>
+  );
+}
